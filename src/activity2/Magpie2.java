f9390cced830c55cd49671f4f44e0840fc608934
tanvinayak6 <tncces1@gmail.com>
2015-02-08 18:18:10 -0700

    Update Magpie2.java

--- src/activity2/Magpie2.java
+++ src/activity2/Magpie2.java
@@ -4,7 +4,7 @@
  * <ul><li>
  *       Uses indexOf to find strings
  * </li><li>
- * 		    Handles responding to simple words and phrases 
+ *       Handles responding to simple words and phrases 
  * </li></ul>
  * This version uses a nested if to handle default responses.
  * @author Laurie White
@@ -12,71 +12,96 @@
  */
 public class Magpie2
 {
-	/**
-	 * Get a default greeting 	
-	 * @return a greeting
-	 */
-	public String getGreeting()
-	{
-		return "Hello, let's talk.";
-	}
-	
-	/**
-	 * Gives a response to a user statement
-	 * 
-	 * @param statement
-	 *            the user statement
-	 * @return a response based on the rules given
-	 */
-	public String getResponse(String statement)
-	{
-		String response = "";
-		if (statement.indexOf("no") >= 0)
-		{
-			response = "Why so negative?";
-		}
-		else if (statement.indexOf("mother") >= 0
-				|| statement.indexOf("father") >= 0
-				|| statement.indexOf("sister") >= 0
-				|| statement.indexOf("brother") >= 0)
-		{
-			response = "Tell me more about your family.";
-		}
-		else
-		{
-			response = getRandomResponse();
-		}
-		return response;
-	}
+ /**
+  * Get a default greeting  
+  * @return a greeting
+  */
+ public String getGreeting()
+ {
+  return "Hello, let's talk.";
+ }
+ 
+ /**
+  * Gives a response to a user statement
+  * 
+  * @param statement
+  *            the user statement
+  * @return a response based on the rules given
+  */
+ public String getResponse(String statement)
+ {
+  String response = "";
+  statement = statement.trim();
+   if( statement.equals(""))
+    {
+      response = "Say something, I'm giving up on you.";
+    }
+  else if (statement.indexOf("no") >= 0)
+  {
+   response = "Why so negative?";
+  }
+  else if (statement.indexOf("mother") >= 0
+    || statement.indexOf("father") >= 0
+    || statement.indexOf("sister") >= 0
+    || statement.indexOf("brother") >= 0)
+  {
+   response = "Tell me more about your family.";
+  }
+     else if(statement.indexOf("dog") >= 0|| statement.indexOf("cat") >= 0)
+   {
+     response = "Tell me more about your pets.";
+   }
+    else if(statement.contains("Mr. Komarinski") )
+    {
+     response ="Koma koma give me a diploma. LOL. Mr. Komarinski is cool";
+   }
+    else if(statement.contains("Joey from Friends")){
+      response = "He is my boyfriend";
+    } 
+    else if(statement.contains("Tanvi")){
+      response = "She is the best";
+    }
+    else if (statement.contains("one direction"))
+    {
+      response = "Zayn is the most beautiful person ever.";
+    }
+    
+ 
+  else
+  {
+   response = getRandomResponse();
+  }
+  return response;
+ }
 
-	/**
-	 * Pick a default response to use if nothing else fits.
-	 * @return a non-committal string
-	 */
-	private String getRandomResponse()
-	{
-		final int NUMBER_OF_RESPONSES = 4;
-		double r = Math.random();
-		int whichResponse = (int)(r * NUMBER_OF_RESPONSES);
-		String response = "";
-		
-		if (whichResponse == 0)
-		{
-			response = "Interesting, tell me more.";
-		}
-		else if (whichResponse == 1)
-		{
-			response = "Hmmm.";
-		}
-		else if (whichResponse == 2)
-		{
-			response = "Do you really think so?";
-		}
-		else if (whichResponse == 3)
-		{
-			response = "You don't say.";
-		}
+ /**
+  * Pick a default response to use if nothing else fits.
+  * @return a non-committal string
+  */
+ private String getRandomResponse()
+ {
+  final int NUMBER_OF_RESPONSES = 4;
+  double r = Math.random();
+  int whichResponse = (int)(r * NUMBER_OF_RESPONSES);
+  String response = "";
+  
+  if (whichResponse == 0)
+  {
+   response = "Interesting, tell me more.";
+  }
+  else if (whichResponse == 1)
+  {
+   response = "Hmmm.";
+  }
+  else if (whichResponse == 2)
+  {
+   response = "Do you really think so?";
+  }
+  else if (whichResponse == 3)
+  {
+   response = "You don't say.";
+  }
 
-		return response;
-	}
+  return response;
+ }
 }
